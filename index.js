3f474b5abdbfaea9a2d7ac7186e3f09092f0e8bd
Aveline Wingfield <avelinewingfield@gmail.com>
2020-09-15 17:28:09 -0400

    too many projects in a row making this from scratch. I'm burnt out.

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const helmet = require('helmet')
 const morgan = require('morgan')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const server = express()
 
 server.use(express.json())
+
+function generateToken({username, role}){
+    const payload = {
+        username, role
+    }
+    const config = {
+        jwtSecret: process.env.JWT_SECRET || 'I like Socks'
+    }
+    const options = {
+        expiresIn = '1d'
+    }
+    return jwt.sign(payload, config.jwtSecret, options)
+}
 
 const port = process.env.PORT || 5000;
 
